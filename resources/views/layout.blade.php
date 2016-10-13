79b000acad9085f794d6938c0f2b63aa51f40a32
KoheiNishioka <k,nishioka@tecotec.co.jp>
2016-10-13 18:15:22 +0900

    Change_20161013# Add chartdiv with JavaScript

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,9 +6,9 @@
 
     <style type="text/css">
 	
-	body {  
-		   text-align: center; 
-	}  	
+	/*body {  
+		   
+	}  	*/
 	
 	div #wrapper {  
     	width: 800px;  
@@ -16,13 +16,22 @@
     	text-align: left;  
     	border: 1px solid #FF0000;  
 	}
-	  
-	</style>
-	<!-- CSSを追加 --><!-- ① 追加 -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 
-</head>
-<body>
+	body { background-color: #30303d; color: #fff; }
+	#chartdiv {
+		width	: 100%;
+		height	: 500px;
+		text-align: center; 
+	}						
+	
+	</style>
+		  
+	
+	<!-- CSSを追加 --><!-- ① 追加 -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+	</head>
+	<body>
 
 	@section('sidebar')
             <!-- This is the master sidebar. -->
@@ -32,5 +41,73 @@
 	<div style="align:center" class="container">
     	@yield('content')
  	</div>
-</body>
+ 	
+ 	<!-- HTML -->
+	<div id="chartdiv"></div>
+
+	</body>
 </html>
+
+
+
+<!-- Resources -->
+<script src="https://www.amcharts.com/lib/3/amcharts.js"></script>
+<script src="https://www.amcharts.com/lib/3/serial.js"></script>
+<script src="https://www.amcharts.com/lib/3/plugins/export/export.min.js"></script>
+<link rel="stylesheet" href="https://www.amcharts.com/lib/3/plugins/export/export.css" type="text/css" media="all" />
+<script src="https://www.amcharts.com/lib/3/themes/dark.js"></script>
+
+<!-- Chart code -->
+<script>
+var chart = AmCharts.makeChart("chartdiv", {
+	"theme": "dark",
+    "type": "serial",
+    "dataProvider": [{
+        "name": "John",
+        "startTime": 8,
+        "endTime": 11,
+        "color": "#FF0F00"
+    }, {
+        "name": "Joe",
+        "startTime": 10,
+        "endTime": 13,
+        "color": "#FF9E01"
+    }, {
+        "name": "Susan",
+        "startTime": 11,
+        "endTime": 18,
+        "color": "#F8FF01"
+    }, {
+        "name": "Eaton",
+        "startTime": 15,
+        "endTime": 19,
+        "color": "#04D215"
+    }],
+    "valueAxes": [{
+        "axisAlpha": 0,
+        "gridAlpha": 0.1
+    }],
+    "startDuration": 1,
+    "graphs": [{
+        "balloonText": "<b>[[category]]</b><br>starts at [[startTime]]<br>ends at [[endTime]]",
+        "colorField": "color",
+        "fillAlphas": 0.8,
+        "lineAlpha": 0,
+        "openField": "startTime",
+        "type": "column",
+        "valueField": "endTime"
+    }],
+    "rotate": true,
+    "columnWidth": 1,
+    "categoryField": "name",
+    "categoryAxis": {
+        "gridPosition": "start",
+        "axisAlpha": 0,
+        "gridAlpha": 0.1,
+        "position": "left"
+    },
+    "export": {
+    	"enabled": true
+     }
+});
+</script>
